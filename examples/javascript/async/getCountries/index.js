8ab2566b53d8f347187945bf5a2699f60d91c1df
Gal Amouyal <galamouy@Gals-MBP-2.lan>
2025-02-02 11:50:32 +0200

    a

--- examples/javascript/async/getCountries/index.js
+++ examples/javascript/async/getCountries/index.js
@@ -3,3 +3,20 @@
 // https://restcountries.com/v3.1/all
 //  1. function async getCountries
 //  2. print all countries names
+
+async function getCountries() {
+  const result = await fetch("https://restcountries.com/v3.1/all");
+  const data = await result.json();
+  return data;
+}
+
+async function init() {
+  try {
+    const result = await getCountries();
+    console.log(result.map((item) => item?.name?.common));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
