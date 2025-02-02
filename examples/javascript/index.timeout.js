1c76d353c866ba3d8d7a625ed136f57d55f46040
Gal Amouyal <galamouy@Gals-MBP-2.lan>
2025-02-02 10:27:47 +0200

    a

---
+++ examples/javascript/index.timeout.js
@@ -0,0 +1,24 @@
+console.log("Do you see this? ");
+console.log("Script start");
+
+setTimeout(() => {
+  console.log("Hamburger for maya is ready");
+}, 6000);
+
+setTimeout(() => {
+  console.log("Coffee for snir is ready ");
+}, 3000);
+
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
+console.log("Script end");
