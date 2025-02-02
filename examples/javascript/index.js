9e92d67fc0e4472189bbf23e495c5832dda8baa2
Gal Amouyal <galamouy@Gals-MBP-2.lan>
2025-02-02 10:10:16 +0200

    a

--- examples/javascript/index.js
+++ examples/javascript/index.js
@@ -1,1 +1,10 @@
 console.log("Do you see this? ");
+console.log("Script start");
+// DONT DO THIS!!! long calculation are not recommended in JS
+for (let index = 0; index < 9999999999; index++) {}
+
+function getUserAndDraw() {
+  console.log("drawing user");
+}
+getUserAndDraw();
+console.log("Script end");
