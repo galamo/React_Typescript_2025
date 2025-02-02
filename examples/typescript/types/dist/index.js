120ae51d73739d1d117f751e3a2b7f79fb113148
Gal Amouyal <galamouy@Gals-MBP-2.lan>
2025-02-02 15:06:59 +0200

    a

--- examples/typescript/types/dist/index.js
+++ examples/typescript/types/dist/index.js
@@ -6,6 +6,7 @@ const tradeSystem = {
     currencies: ["BTC", "ETH"],
     createdAt: new Date().toISOString(),
     numberOfUsers: 999,
+    sales: [],
 };
 const user = 112313;
 async function getCountries() {
@@ -17,4 +18,20 @@ async function init() {
     const result = await getCountries();
     console.log(result.map((item) => item?.name?.common));
 }
+var COINS;
+(function (COINS) {
+    COINS[COINS["ETH"] = 1] = "ETH";
+    COINS[COINS["BTC"] = 2] = "BTC";
+    COINS[COINS["SOL"] = 3] = "SOL";
+})(COINS || (COINS = {}));
+let up = {
+    serial: "string",
+    code: "string",
+    createdAt: "string",
+    balance: 222,
+    coin: COINS.ETH,
+    branch: "someBranch",
+};
+const coinApi = { value: 171 };
+function searchCountry() { }
 init();
--- examples/typescript/types/src/index.ts
+++ examples/typescript/types/src/index.ts
@@ -17,11 +17,16 @@ interface ITradeSystem {
   numberOfUsers: number;
 }
 
+interface ITradeSystem {
+  sales: Array<string>;
+}
+
 const tradeSystem: ITradeSystem = {
   title: "tarde-in",
   currencies: ["BTC", "ETH"],
   createdAt: new Date().toISOString(),
   numberOfUsers: 999,
+  sales: [],
 };
 const user: number = 112313;
 
@@ -35,4 +40,43 @@ async function init() {
   const result = await getCountries();
   console.log(result.map((item) => item?.name?.common));
 }
+
+type CoinApi = Partial<Coin>;
+type CoinApi2 = Required<{ id?: string }>;
+
+type Wallet = {
+  serial: string;
+  code: string;
+  createdAt: string;
+  balance: number;
+  coin: COINS;
+};
+
+type Account = {
+  owner: string;
+  bank: string;
+  branch: string;
+};
+
+enum COINS {
+  ETH = 1,
+  BTC = 2,
+  SOL = 3,
+}
+
+type UserProfile = Wallet & Account;
+type UserProfileUn = Wallet | Account;
+let up: UserProfileUn = {
+  serial: "string",
+  code: "string",
+  createdAt: "string",
+  balance: 222,
+  coin: COINS.ETH,
+  branch: "someBranch",
+};
+
+const coinApi: CoinApi = { value: 171 };
+
+function searchCountry() {}
+
 init();
